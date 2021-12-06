5f632972ce3e300691a4592be38c3c43d6557b65
Kseniya Melnik <kseniyamelnnik@gmail.com>
2021-12-06 21:38:36 +0300

    feat: add main block in portfolio

---
+++ src/main/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import style from './Main.module.css';
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+        <div className={style.container}>
+            <div className={style.text}>
+                <span>Hi There</span>
+                <h1>I am Kseniya Melnik</h1>
+                <p>Frontend developer</p>
+            </div>
+            <div className={style.photo}></div>
+        </div>
+        </div>
+    );
+}
+
+export default Main;
