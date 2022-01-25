334e607b468fa14a7cd38d8a7014db05d45967ae
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-01-25 20:02:57 +0300

    feat: add styles to main block

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,24 @@
 import React from "react";
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/style.module.css'
+import avatar from "../assets/image/avatar.jpg"
+
 
 function Main() {
+    const avatarStyle = {
+        backgroundImage: `url(${avatar})`,
+    };
     return (
         <div className={style.main}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.container}`}>
                 <div className={style.text}>
                     <span>Hi There</span>
-                    <h1>I am Kseniya Melnik</h1>
-                    <p>Frontend developer</p>
+                    <h1 className={style.title}>I am Kseniya Melnik</h1>
+                    <span>Frontend developer</span>
                 </div>
-                <div className={style.photo}></div>
+                <div style={avatarStyle} className={style.photo}>
+                </div>
+
             </div>
         </div>
     );
