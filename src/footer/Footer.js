535c1f8a6be88e040c122f062a8582bd6ff27615
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-01-26 13:58:25 +0300

    feat: add styles to footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,20 +1,40 @@
 import React from "react";
 import style from './Footer.module.scss';
 import styleContainer from "../common/styles/style.module.css";
+import codewars from "../assets/image/codewars.svg";
+import github from "../assets/image/github.svg";
+import linkedin from "../assets/image/linkedin.svg";
+import telegram from "../assets/image/telegram.svg";
+
+
 
 function Footer() {
+    const codewarsStyle = {
+        backgroundImage: `url(${codewars})`,
+    };
+    const githubStyle = {
+        backgroundImage: `url(${github})`,
+    };
+    const linkedinStyle = {
+        backgroundImage: `url(${linkedin})`,
+    };
+    const telegramStyle = {
+        backgroundImage: `url(${telegram})`,
+    };
+
+
     return (
         <div className={style.footer}>
             <div className={`${styleContainer.container} ${style.container}`}>
                     <h2 className={style.title}>Kseniya Melnik</h2>
                 <div className={style.block}>
-                    <div className={style.fragment}></div>
-                    <div className={style.fragment}></div>
-                    <div className={style.fragment}></div>
-                    <div className={style.fragment}></div>
+                    <div style = {githubStyle} className={style.social}></div>
+                    <div style = {linkedinStyle} className={style.social}></div>
+                    <div style = {telegramStyle} className={style.social}></div>
+                    <div style = {codewarsStyle} className={style.social}></div>
                 </div>
                 <div >
-                    <span className={style.copyright}> @2021 Все права защищены</span>
+                    <span className={style.copyright}> @2021 all rights reserved </span>
                 </div>
             </div>
         </div>
