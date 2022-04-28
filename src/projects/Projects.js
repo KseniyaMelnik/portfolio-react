4892a6501d23e93cf13884c070d7b078f2a5cd54
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-04-28 20:50:57 +0300

    feat: add new project to the portfolio

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -7,6 +7,7 @@ import todoImage from './../assets/image/todolist.jpg';
 import socialImage from './../assets/image/sotial-network.jpg'
 import wildImage from './../assets/image/wildlife.jpg'
 import drumImage from './../assets/image/drumkit.jpg'
+import shelterImage from './../assets/image/shelter.jpg'
 
 function Projects() {
     const socialStyle = {
@@ -21,6 +22,9 @@ function Projects() {
     const drumStyle = {
         backgroundImage: `url(${drumImage})`,
     };
+    const shelterStyle = {
+        backgroundImage: `url(${shelterImage})`,
+    };
 
 
     return (
@@ -32,7 +36,7 @@ function Projects() {
                     <Project url={"https://kseniyamelnik.github.io/todo-list/#/login"} style={todoStyle} title={"Todo list"} description={"It's something description for the project"}/>
                     <Project url={"https://rolling-scopes-school.github.io/kseniyamelnik-JSFE2021Q1/wildlife/"} style={wildStyle} title={"Wild life"} description={"It's something description for the project"}/>
                     <Project url={"https://rolling-scopes-school.github.io/kseniyamelnik-JSFEPRESCHOOL/drum-kit/"}style={drumStyle} title={"Drum kit"} description={"It's something description for the project"}/>
-
+                    <Project url={"https://rolling-scopes-school.github.io/kseniyamelnik-JSFE2022Q1/shelter/main/"}style={shelterStyle} title={"Shelter"} description={"It's something description for the project"}/>
                 </div>
             </div>
         </div>
